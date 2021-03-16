2ec24c0d246ca95d2b4152e7fb9f04c71e4fdaf5
AleksejRonzhin <aleksej.ronzhin.rsreu@gmail.com>
2021-03-16 23:11:05 +0300

    Added client.executeQuery without arg and exceptions

--- SQL/src/main/kotlin/AppRun.kt
+++ SQL/src/main/kotlin/AppRun.kt
@@ -6,6 +6,16 @@ fun main(){
 
     Init.createTables(client)
     Init.insertInto(client)
+
+    val res = client.executeQuery("""
+        SELECT *
+            FROM Book
+    """.trimIndent())
+    println(res[0]["name"])
+    println(res[1]["name"])
+    println(res[2]["name"])
+    println(res[3]["name"])
+
     Init.dropTable(client)
 
     client.close()
--- SQL/src/main/kotlin/Client.kt
+++ SQL/src/main/kotlin/Client.kt
@@ -5,10 +5,34 @@ class Client(url : String) {
     val conn = DriverManager.getConnection("jdbc:sqlite:.$url")
 
     fun executeUpdate(sql: String){
-        conn.createStatement().executeUpdate(sql)
+        this.conn.createStatement().executeUpdate(sql)
+    }
+
+    fun executeQuery(sql : String): List <HashMap<String, Any>> {
+
+        val resList = mutableListOf<HashMap<String, Any>>()
+
+        val preparedStatement = this.conn.prepareStatement(sql)
+
+        val res  = preparedStatement.executeQuery()
+
+        val meta = preparedStatement.metaData
+
+        while(res.next()) {
+            val map = hashMapOf<String, Any>()
+            var attributeName: String
+            var i = 1
+            while (i <= meta.columnCount){
+                attributeName = meta.getColumnName(i)
+                map[attributeName] = res.getObject(i)
+                i++
+            }
+            resList.add(map)
+        }
+
+        return resList
     }
 
 
-
-    fun close() = conn.close()
+    fun close() = this.conn.close()
 }
