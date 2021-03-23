de60319f4471ede462e6033dcbc4fa30007a6f9e
AleksejRonzhin <aleksej.ronzhin.rsreu@gmail.com>
2021-03-23 19:02:17 +0300

    Remade Stack.kt

--- Collections1/src/main/kotlin/Queue.kt
+++ Collections1/src/main/kotlin/Queue.kt
@@ -1,19 +1,19 @@
 import java.util.LinkedList
 
 class Queue<T> {
-    private val list: LinkedList<T?> = LinkedList()
+	private val list: LinkedList<T?> = LinkedList()
 
-    fun isEmpty(): Boolean = this.list.isEmpty()
+	fun isEmpty(): Boolean = this.list.isEmpty()
 
-    fun enqueue(element: T) = this.list.add(element)
+	fun enqueue(element: T) = this.list.add(element)
 
-    fun dequeue(): T? = this.list.removeFirstOrNull()
+	fun dequeue(): T? = this.list.removeFirstOrNull()
 }
 
 fun <T> queueOf(vararg elements: T): Queue<T> {
-    val queue = Queue<T>()
-    elements.forEach {
-        queue.enqueue(it)
-    }
-    return queue
+	val queue = Queue<T>()
+	elements.forEach {
+		queue.enqueue(it)
+	}
+	return queue
 }
--- Collections1/src/main/kotlin/Stack.kt
+++ Collections1/src/main/kotlin/Stack.kt
@@ -1,20 +1,19 @@
+import java.util.LinkedList
+
 class Stack<T> {
+	private val list: LinkedList<T?> = LinkedList()
 
-    private val list = mutableListOf<T>()
+	fun isEmpty(): Boolean = this.list.isEmpty()
 
-    fun isEmpty(): Boolean = this.list.isEmpty()
+	fun push(element: T) = this.list.add(element)
 
-    fun push(element: T){
-        this.list.add(element)
-    }
-
-    fun pop(): T? = this.list.removeLastOrNull()
+	fun pop(): T? = this.list.removeLastOrNull()
 }
 
 fun <T> stackOf(vararg elements: T): Stack<T> {
-    val stack = Stack<T>()
-    elements.forEach {
-        stack.push(it)
-    }
-    return stack
+	val stack = Stack<T>()
+	elements.forEach {
+		stack.push(it)
+	}
+	return stack
 }
