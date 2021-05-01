151cbda7cdfdd68fc185d46ec800cc311d6d4b29
Алексей <aleksej.ronzhin.rsreu@gmail.com>
2021-05-01 21:56:01 +0300

    Remade methods

--- Coroutines/src/main/kotlin/AppRunner.kt
+++ Coroutines/src/main/kotlin/AppRunner.kt
@@ -1,17 +1,17 @@
-import DAO.BookDAO
-import kotlinx.coroutines.*
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
-fun main() = runBlocking{
-	println(AuthorWithBooksService.getAuthorWithBooks(1))
-	val channel = Channel<String>()
-	val bookService = BookService(channel, BookDAO.getAllBooks(),2000)
-	val job = launch{
-		bookService.generateBooks()
-	}
-	repeat(5) {
-		println(bookService.getRandomBookInfo())
-	}
-	job.cancelAndJoin()
-
+suspend fun main() = coroutineScope {
+    println(AuthorWithBooksService.getAuthorWithBooks(1))
+    val channel = Channel<String>()
+    val bookService = BookService(channel, 2000)
+    val job = launch {
+        bookService.generateBooks()
+    }
+    repeat(5) {
+        println(bookService.getRandomBookInfo())
+    }
+    job.cancelAndJoin()
 }
--- Coroutines/src/main/kotlin/BookService.kt
+++ Coroutines/src/main/kotlin/BookService.kt
@@ -1,23 +1,24 @@
+import DAO.BookDAO
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import models.Book
 import java.lang.Math.random
 
 class BookService(
-	private val channel: Channel<String>,
-	private val bookList: List<Book>,
-	private val delay: Long,
+    private val channel: Channel<String>,
+    private val delay: Long,
+    private val bookList: List<Book> = BookDAO.getAllBooks()
 ) {
 
-	suspend fun generateBooks() {
-		while (true) {
-			val numberBook = (random() * bookList.size).toInt()
-			channel.send(bookList[numberBook].toString())
-			delay(delay)
-		}
-	}
+    suspend fun generateBooks() {
+        while (true) {
+            val numberBook = (random() * bookList.size).toInt()
+            channel.send(bookList[numberBook].toString())
+            delay(delay)
+        }
+    }
 
-	suspend fun getRandomBookInfo(): String {
-		return channel.receive()
-	}
+    suspend fun getRandomBookInfo(): String {
+        return channel.receive()
+    }
 }
